3eba28ec4a4352f26fc560a6609987f4cab0acdb
Gery <gguij002@gmail.com>
2017-08-16 20:09:37 -0400

    First Commit

---
+++ app/src/main/java/com/gery/localinsta/LiApplication.java
@@ -0,0 +1,30 @@
+package com.gery.localinsta;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by gguij002 on 6/21/17.
+ */
+
+public class LiApplication extends Application {
+
+    private static LiApplication context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        context = this;
+
+//        Realm.init(context);
+//        RealmConfiguration realmConfig = new RealmConfiguration.Builder()
+//                .build();
+//
+//        Realm.setDefaultConfiguration(realmConfig);
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+}
